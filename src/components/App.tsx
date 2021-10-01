654f547b612e757bfeca1b347e3cfc7fdb582ff6
= <=>
2021-10-01 14:29:33 +0900

    Render Match component in App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {Match} from './Match';
+import {mMatch} from '~/models/mMatch';
 
 function App() {
+  const [match,setMatch]=useState({} as mMatch); // TODO: 有効なオブジェクトを渡す手段を考える
   return (
     <div className="App">
+      <Match match={match} />
     </div>
   );
 }
