81fdfa6834d11e4297f927ec507340ea9e163106
= <=>
2021-10-02 23:45:10 +0900

    Change Match data source to Redux

--- src/components/Match.tsx
+++ src/components/Match.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import {mMatch} from 'models/mMatch'
+import {useSelector} from 'react-redux';
+import {RootState} from 'stores/index';
+import {mMatch} from 'models/mMatch';
 import {Side} from './Side';
+import { match_selectors } from 'stores/slices/match';
 
 type HeaderProps = {
   metadata: Omit<mMatch,'content'>
@@ -14,12 +17,16 @@ const MatchHeader: React.VFC<HeaderProps> = (props)=>(
 );
 
 type Props = {
-  match: mMatch;
+  matchID: string;
 }
 
-export const Match: React.VFC<Props> = (props)=>(
-  <div className='match'>
-    <MatchHeader metadata={props.match} />
-    {props.match.contents?.map(side=>(<Side side={side} />))??null}
-  </div>
-);
+export const Match: React.VFC<Props> = (props)=>{
+  const match=useSelector((state:RootState)=>match_selectors.selectById(state,props.matchID));
+  if(match===undefined) return null;
+  return (
+    <div className='match'>
+      <MatchHeader metadata={match} />
+      {match.contents?.map(side=>(<Side sideID={side} />))??null}
+    </div>
+  );
+}
