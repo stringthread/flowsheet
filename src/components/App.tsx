3a93866ed0174ce43ad191470bce165f8c148a12
= <=>
2021-10-02 23:46:32 +0900

    Wrap App with `Provider` and manage match with ID

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,16 @@
 import React,{useState} from 'react';
+import {Provider} from 'react-redux';
+import {store} from 'stores/index';
 import {Match} from './Match';
-import {mMatch} from 'models/mMatch';
 
 function App() {
-  const [match,setMatch]=useState({} as mMatch); // TODO: 有効なオブジェクトを渡す手段を考える
+  const [matchID,setMatchID]=useState('DUMMY_MATCH_ID'); // TODO: 有効なオブジェクトを渡す手段を考える
   return (
-    <div className="App">
-      <Match match={match} />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Match matchID={matchID} />
+      </div>
+    </Provider>
   );
 }
 
