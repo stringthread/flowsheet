47977db446e3c03423aa7862881274a2b8939734
= <=>
2021-10-02 20:12:34 +0900

    Create side Slice

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -3,12 +3,14 @@ import { TypedUseSelectorHook, useSelector as rawUseSelector } from 'react-redux
 import {evidence_slice} from './slices/evidence';
 import {point_slice} from './slices/point';
 import {part_slice} from './slices/part';
+import {side_slice} from './slices/side';
 
 export const store=configureStore({
   reducer: {
     evidence: evidence_slice.reducer,
     point: point_slice.reducer,
     part: part_slice.reducer,
+    side: side_slice.reducer,
   }
 });
 
---
+++ src/stores/slices/side.ts
@@ -0,0 +1,33 @@
+import {createEntityAdapter,createSlice,PayloadAction} from '@reduxjs/toolkit';
+import {RootState} from '../index';
+import {EntityStateWithLastID} from './EntityStateWithLastID';
+import {mSide} from 'models/mSide';
+
+const side_adapter=createEntityAdapter<mSide>();
+const side_initialState:EntityStateWithLastID<mSide>=side_adapter.getInitialState({
+  last_id_number: 0
+});
+export const side_slice=createSlice({
+  name: 'side',
+  initialState: side_initialState,
+  reducers: {
+    add: (state,action:PayloadAction<mSide>)=>{
+      side_adapter.addOne(state,action.payload);
+    },
+    removeOne: (state,action:PayloadAction<string>)=>{
+      side_adapter.removeOne(state,action.payload);
+    },
+    removeAll: state=>{
+      side_adapter.removeAll(state);
+    },
+    // Payload[a,b]->ID==aの要素にあるcontentsの末尾にbを追加する
+    addChild: (state,action:PayloadAction<[string,string]>)=>{
+      const [id, new_part]=action.payload;
+      const entity=state.entities[id];
+      if(entity===undefined) return;
+      if(entity.contents===undefined) entity.contents=[];
+      entity.contents.push(new_part);
+    },
+  }
+});
+export const side_selectors=side_adapter.getSelectors<RootState>(state=>state.side);
