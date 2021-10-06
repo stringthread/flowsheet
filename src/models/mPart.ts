8825cdcb5e38885f1f1287f6ad8cdc32163fa4be
= <=>
2021-10-06 20:40:52 +0900

    generate_part()に初期コンテンツ生成処理を追加

--- src/models/mPart.ts
+++ src/models/mPart.ts
@@ -1,6 +1,7 @@
 import {store} from 'stores';
 import {part_slice} from 'stores/slices/part';
 import {generate_part_id} from 'stores/slices/id_generators';
+import {generate_point} from 'models/mPoint';
 
 export interface mPart {
   id: string;
@@ -11,9 +12,11 @@ export interface mPart {
 export const generate_part=(
   from?:Omit<mPart,'id'|'contents'>,
 ):mPart=>{
+  const child_point=generate_point();// NOTE: 仮実装
   const generated: mPart= {
     ...from,
-    id: generate_part_id()
+    id: generate_part_id(),
+    contents: [child_point.id], // NOTE: 仮実装
   };
   store.dispatch(part_slice.actions.add(generated));
   return generated;
