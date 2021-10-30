d1a76dba7e4e902a37b527c20a350857746c2892
= <=>
2021-10-30 23:31:25 +0900

    Partにスタイルを適用

--- src/components/Part.tsx
+++ src/components/Part.tsx
@@ -1,5 +1,6 @@
 import React,{useCallback} from 'react';
 import {useSelector} from 'react-redux';
+import {css} from '@emotion/react';
 import {RootState} from 'stores/index';
 import {Point} from './Point';
 import {typeSelected} from './App';
@@ -10,6 +11,18 @@ type Props = {
   setSelected: (_:typeSelected)=>void;
 };
 
+const stylePartName=css`
+  border-bottom: solid 1px black;
+  padding: 0.5em;
+  font-weight: bold;
+`;
+const stylePartChildrenWrap=css`
+  padding: 1em;
+`;
+const stylePart=css`
+  border: solid 1px black;
+`;
+
 export const Part: React.VFC<Props> = (props)=>{
   const part=useSelector((state:RootState)=>part_selectors.selectById(state,props.partID));
   const onClick=useCallback((e: React.MouseEvent)=>{
@@ -19,11 +32,13 @@ export const Part: React.VFC<Props> = (props)=>{
   },[props.partID,props.setSelected]);
   if(part===undefined) return null;
   return (
-    <div className="part" data-testid="part" onClick={onClick}>
-      <div className="partName">{part.name??''}</div>
-      {part.contents?.map(
-        content=><Point pointID={content} setSelected={props.setSelected} />
-       )??null}
+    <div className="part" data-testid="part" onClick={onClick} css={stylePart}>
+      <div className="partName" css={stylePartName}>{part.name??''}</div>
+      <div className="partChildrenWrap" css={stylePartChildrenWrap}>
+        {part.contents?.map(
+          content=><Point pointID={content} setSelected={props.setSelected} />
+         )??null}
+       </div>
     </div>
   );
 }
