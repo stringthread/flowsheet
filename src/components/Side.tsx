aedc43da90316cd97a3cec0e233817e73d2ba8ad
= <=>
2021-10-30 23:42:20 +0900

    Sideに基本的なスタイルを適用

--- src/components/Side.tsx
+++ src/components/Side.tsx
@@ -1,5 +1,6 @@
 import React,{useCallback} from 'react';
 import {useSelector} from 'react-redux';
+import {css} from '@emotion/react';
 import {RootState} from 'stores/index';
 import {Part} from './Part';
 import {typeSelected} from './App';
@@ -10,6 +11,19 @@ type Props = {
   setSelected: (_:typeSelected)=>void;
 }
 
+const styleSide=css``;
+const styleSideName=css``;
+const styleSideChildrenWrap=css`
+  width: 100%;
+  min-height: 20em;
+  display: flex;
+  gap: 1em;
+  justify-content: space-between;
+  &>*{
+    width: 100%; /* 子要素を強制的に均等幅にする */
+  }
+`;
+
 export const Side: React.VFC<Props> = (props)=>{
   const side=useSelector((state:RootState)=>side_selectors.selectById(state,props.sideID));
   const onClick=useCallback((e: React.MouseEvent)=>{
@@ -19,9 +33,11 @@ export const Side: React.VFC<Props> = (props)=>{
   },[props.sideID,props.setSelected]);
   if(side===undefined) return null;
   return (
-    <div className="side" data-testid='side' onClick={onClick}>
-      <div className="sideName">{side.side}</div>
-      {side.contents?.map(content=><Part partID={content} setSelected={props.setSelected} />)??null}
+    <div className="side" data-testid='side' onClick={onClick} css={styleSide}>
+      <div className="sideName" css={styleSideName}>{side.side}</div>
+      <div className="sideChildrenWrap" css={styleSideChildrenWrap}>
+        {side.contents?.map(content=><Part partID={content} setSelected={props.setSelected} />)??null}
+      </div>
     </div>
   );
 };
