bb3ea82770435d9a61fd82028594e32958fbed10
= <=>
2021-10-30 23:23:27 +0900

    Pointにスタイルを適用

--- src/components/Point.tsx
+++ src/components/Point.tsx
@@ -1,5 +1,6 @@
 import React,{useCallback} from 'react';
 import {useSelector,useDispatch} from 'react-redux';
+import {css} from '@emotion/react';
 import {RootState} from 'stores/index';
 import {mPoint} from 'models/mPoint';
 import {Evidence} from './Evidence'
@@ -18,12 +19,18 @@ type ChildProps = {
   setSelected: (_:typeSelected)=>void;
 };
 
+const stylePointClaim=css`
+  width: 100%;
+  border: none !important;
+`;
+
 const PointChild: React.VFC<ChildProps> = (props)=>{
   const dispatch=useDispatch();
   if(typeof props.contents === 'string'){
     return (
       <TextArea
         className="pointClaim"
+        placeholder=" "
         value={props.contents}
         onBlur={(e)=>{
           dispatch(point_slice.actions.upsertOne({
@@ -31,6 +38,7 @@ const PointChild: React.VFC<ChildProps> = (props)=>{
             contents: e.currentTarget.value,
           }));
         }}
+        css={stylePointClaim}
       />
     );
   }
@@ -43,6 +51,23 @@ const PointChild: React.VFC<ChildProps> = (props)=>{
   );
 }
 
+const stylePointNumbering=css`
+  display: inline-block;
+  width: 1em;
+  height: 1em;
+`;
+
+const stylePointChildrenWrap=css`
+  flex-grow: 1;
+`;
+
+const stylePoint=css`
+  width: 100%;
+  display: flex;
+  column-gap: 0.5em;
+  margin-bottom: 0.5em;
+`;
+
 export const Point: React.VFC<Props> = (props)=>{
   const dispatch=useDispatch();
   const point=useSelector((state:RootState)=>point_selectors.selectById(state,props.pointID));
@@ -53,7 +78,7 @@ export const Point: React.VFC<Props> = (props)=>{
   },[props.pointID,props.setSelected]);
   if(point===undefined) return null;
   return (
-    <div className="point" data-testid="point" onClick={onClick}>
+    <div className="point" data-testid="point" onClick={onClick} css={stylePoint}>
       <TextInput
         className="pointNumbering"
         data-testid="pointNumbering"
@@ -64,8 +89,11 @@ export const Point: React.VFC<Props> = (props)=>{
             numbering: e.currentTarget.value,
           }));
         }}
+        css={stylePointNumbering}
       />
-      {point.contents!==undefined?<PointChild parentID={props.pointID} contents={point.contents} setSelected={props.setSelected} />:null}
+      <div className="pointChildrenWrap" data-testid="pointChildrenWrap" css={stylePointChildrenWrap}>
+        {point.contents!==undefined?<PointChild parentID={props.pointID} contents={point.contents} setSelected={props.setSelected} />:null}
+      </div>
     </div>
   );
 };
