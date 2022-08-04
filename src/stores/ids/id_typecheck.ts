62b422da9c3caffe23d7c9a432cdf27664c30002
stringthread <string.thread@gmail.com>
2022-08-05 00:31:52 +0900

    IDからの型判定関数を定義

---
+++ src/stores/ids/id_typecheck.ts
@@ -0,0 +1,17 @@
+import {ID_TYPE, evidence_id_prefix, point_id_prefix, part_id_prefix, side_id_prefix, match_id_prefix} from './id_generators'
+
+export const id_is_mMatch=(id:ID_TYPE):boolean=>{
+  return id.startsWith(match_id_prefix);
+}
+export const id_is_mSide=(id:ID_TYPE):boolean=>{
+  return id.startsWith(side_id_prefix);
+}
+export const id_is_mPart=(id:ID_TYPE):boolean=>{
+  return id.startsWith(part_id_prefix);
+}
+export const id_is_mPoint=(id:ID_TYPE):boolean=>{
+  return id.startsWith(point_id_prefix);
+}
+export const id_is_mEvidence=(id:ID_TYPE):boolean=>{
+  return id.startsWith(evidence_id_prefix);
+}
