664f8e5a375d0164663f7791c5e95e4610368f55
stringthread <string.thread@gmail.com>
2022-08-13 13:34:30 +0900

    テスト用のconsole.log除去

--- src/services/point.ts
+++ src/services/point.ts
@@ -60,7 +60,6 @@ export const append_point_to_part=(parent_id: baseModel['id']): mPoint|undefined
   const point_store=store.getState().point
   let _parent_id: baseModel['id']|undefined=parent_id;
   while(_parent_id!==undefined && id_is_mPoint(_parent_id)){
-    console.log(_parent_id);
     _parent_id=point_store.entities[_parent_id]?.parent;
   }
   let child:mPoint|undefined=undefined;
