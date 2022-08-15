9111459ea610ccca0a8e4e157a313729891370e5
stringthread <string.thread@gmail.com>
2022-08-15 17:25:54 +0900

    ModelとIDのベース型を定義

--- src/models/baseModel.ts
+++ src/models/baseModel.ts
@@ -1,10 +1,48 @@
-declare const idSymbol: unique symbol;
-export type BASE_ID_TYPE=string&{[idSymbol]: never};
-export const is_ID_TYPE=(value: string): value is BASE_ID_TYPE=>true;
-export const to_ID_TYPE=(seed:string): BASE_ID_TYPE => seed as BASE_ID_TYPE;
+import { Slice } from "@reduxjs/toolkit";
+import { EntityStateWithLastID } from "stores/slices/EntityStateWithLastID";
 
-export interface baseModel{
+export interface rawBaseModel{
   type_signature: string;
-  id: BASE_ID_TYPE;
+  id: string;
   contents?: unknown;
+}
+
+export abstract class baseModelId<T extends BaseModel<rawBaseModel> = BaseModel<rawBaseModel>>{
+  readonly plainId: string;
+  constructor(id: string){
+    this.plainId=id;
+  };
+  abstract getStore(): EntityStateWithLastID<T>;
+  getObj(): T|undefined{
+    return this.getStore().entities[this.plainId];
+  }
+};
+
+export type methodOptions = {
+  [k: string]: any;
+};
+// T: 対応するrawModelの型
+export abstract class BaseModel<T extends rawBaseModel = rawBaseModel>{
+  id: baseModelId<BaseModel<T>>;
+  obj: Omit<T,'id'|'type_signature'>|undefined;
+  constructor(id: baseModelId<BaseModel<T>>);
+  constructor(option?: methodOptions, from?:Partial<T>);
+  constructor(arg1?: methodOptions|baseModelId<BaseModel<T>>, arg2?: Partial<T>){
+    if(arg1 instanceof baseModelId<BaseModel<T>>){
+      this.id=arg1;
+      this.obj=arg1.getObj()?.obj;
+    } else {
+      const generated=this.generate(arg1, arg2);
+      this.id=generated.id;
+      this.obj=generated.obj;
+    }
+  }
+  abstract generate(option?: methodOptions, from?:Partial<T>): BaseModel<T>;
+  abstract getSlice(): Slice<EntityStateWithLastID<rawBaseModel>>;
+  abstract upsert(obj: typeof this.obj): boolean;
+  getContents(): T['contents']|undefined { return this.obj?.contents; }
+  addChild: (()=>baseModelId)|undefined = undefined;
+  reorderChild: (()=>boolean)|undefined = undefined;
+  getParent: (()=>BaseModel|undefined)|undefined = undefined;
+  setParent: (()=>boolean)|undefined = undefined;
 }
