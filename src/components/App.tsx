192167c318968febaa27a6909d39f799e2b66379
stringthread <string.thread@gmail.com>
2022-08-22 00:06:54 +0900

    App内handler処理の切り出し

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React,{useState,useCallback,useLayoutEffect} from 'react';
+import React,{useState, useLayoutEffect, useCallback } from 'react';
 import {Provider} from 'react-redux';
 import {store} from 'stores/index';
 import {point_slice} from 'stores/slices/point'
@@ -7,13 +7,60 @@ import {Match} from './Match';
 import {mPoint} from 'models/mPoint';
 import { mEvidenceSignature } from 'models/mEvidence';
 import {generate_match} from 'services/match';
-import {point_add_child, append_sibling_point, append_point_to_part, append_point_child} from 'services/point';
-
-import {useHotkeys} from 'react-hotkeys-hook';
+import { useHotkeys } from 'react-hotkeys-hook';
 import { append_claim } from 'services/claim';
 import { append_evidence } from 'services/evidence';
+import { append_sibling_point, append_point_child, append_point_to_part } from 'services/point';
 
 export type typeSelected=string|undefined;
+
+const useAppEventListeners = (selected: typeSelected)=>{
+  const add_claim=useCallback((e?:Event|React.SyntheticEvent)=>{
+    e?.preventDefault();
+    if(selected==undefined) return;
+    append_claim(selected);
+  }, [selected]);
+  const add_point=useCallback((e?:Event|React.SyntheticEvent)=>{
+    e?.preventDefault();
+    if(selected==undefined) return;
+    append_sibling_point(selected);
+  }, [selected]);
+  const add_point_child=useCallback((e?:Event|React.SyntheticEvent)=>{
+    e?.preventDefault();
+    if(selected==undefined) return;
+    append_point_child(selected);
+  }, [selected]);
+  const add_point_to_part=useCallback((e?:Event|React.SyntheticEvent)=>{
+    e?.preventDefault();
+    if(selected==undefined) return;
+    append_point_to_part(selected);
+  }, [selected]);
+  const draw_line=useCallback((e?:Event|React.SyntheticEvent)=>{
+    e?.preventDefault();
+    if(selected==undefined) return;
+    // TODO: 実装は後日
+  }, [selected]);
+  const add_evidence=useCallback((e?:Event|React.SyntheticEvent)=>{
+    e?.preventDefault();
+    if(selected==undefined) return;
+    append_evidence(selected);
+  }, [selected]);
+  return {add_claim, add_point, add_point_child, add_point_to_part, draw_line, add_evidence};
+};
+
+type typeHotkeys = { [keys: string]: ReturnType<typeof useHotkeys>; };
+
+const useAppHotkeys = (selected: typeSelected): typeHotkeys=>{
+  const {add_claim, add_point, add_point_child, add_point_to_part, draw_line, add_evidence} = useAppEventListeners(selected);
+  return {
+    'alt+c': useHotkeys('alt+c', add_claim, { enableOnTags: ['INPUT','TEXTAREA'] }),
+    'alt+e': useHotkeys('alt+e', add_evidence, { enableOnTags: ['INPUT','TEXTAREA'] }),
+    'alt+p': useHotkeys('alt+p', add_point, { enableOnTags: ['INPUT','TEXTAREA'] }),
+    'alt+shift+p': useHotkeys('alt+shift+p', add_point_to_part, { enableOnTags: ['INPUT','TEXTAREA'] }),
+    'alt+l': useHotkeys('alt+l', draw_line, { enableOnTags: ['INPUT','TEXTAREA'] }),
+    'alt+ctrl+p': useHotkeys('alt+ctrl+p', add_point_child, { enableOnTags: ['INPUT','TEXTAREA'] }),
+  };
+};
 
 function App() {
   const [matchID,setMatchID]=useState<string>('');
@@ -25,43 +72,8 @@ function App() {
     }).id); // TODO: sideの構成をハードコーディングしているため、設定用Repositoryなどに切り出す
   },[]);
   const [selected, setSelected]=useState<typeSelected>(undefined);
-  const add_claim=(e?:Event|React.SyntheticEvent)=>{
-    e?.preventDefault();
-    if(selected==undefined) return;
-    append_claim(selected);
-  };
-  const add_point=(e?:Event|React.SyntheticEvent)=>{
-    e?.preventDefault();
-    if(selected==undefined) return;
-    append_sibling_point(selected);
-  };
-  const add_point_child=(e?:Event|React.SyntheticEvent)=>{
-    e?.preventDefault();
-    if(selected==undefined) return;
-    append_point_child(selected);
-  };
-  const add_point_to_part=(e?:Event|React.SyntheticEvent)=>{
-    e?.preventDefault();
-    if(selected==undefined) return;
-    append_point_to_part(selected);
-  };
-  const draw_line=(e?:Event|React.SyntheticEvent)=>{
-    e?.preventDefault();
-    if(selected==undefined) return;
-    // TODO: 実装は後日
-  };
-  const add_evidence=(e?:Event|React.SyntheticEvent)=>{
-    e?.preventDefault();
-    if(selected==undefined) return;
-    append_evidence(selected);
-  };
-  useHotkeys('alt+c', add_claim, { enableOnTags: ['INPUT','TEXTAREA'] });
-  useHotkeys('alt+e', add_evidence, { enableOnTags: ['INPUT','TEXTAREA'] });
-  useHotkeys('alt+p', add_point, { enableOnTags: ['INPUT','TEXTAREA'] });
-  useHotkeys('alt+shift+p', add_point_to_part, { enableOnTags: ['INPUT','TEXTAREA'] });
-  useHotkeys('alt+l', draw_line, { enableOnTags: ['INPUT','TEXTAREA'] });
-  useHotkeys('alt+ctrl+p', add_point_child, { enableOnTags: ['INPUT','TEXTAREA'] });
-  
+  const {add_claim, add_point, add_point_to_part, add_evidence}=useAppEventListeners(selected);
+  useAppHotkeys(selected);
   return (
     <Provider store={store}>
       <div className="App">
